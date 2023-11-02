4701c55759f575c88ddb46616fb9b9bdc622dab1
Leonardo Fragozo <leo.fragozo.dev@gmail.com>
2023-11-02 13:09:14 -0300

    improve routes and controllers

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class EventsController < ApplicationController
-  before_action :authenticate_user!, only: %i[new create]
+  before_action :authenticate_user!, only: %i[new create show]
 
   def index
     @events = Event.all
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'events/index'
-  get 'events/show'
-  get 'events/new'
-  get 'events/edit'
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  root "users#index"
+
   resources :events
 end
